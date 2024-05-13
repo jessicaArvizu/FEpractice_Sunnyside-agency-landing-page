342533a2456e2fda7a7709328de419bd82c6cf41
Jessica Arvizu <jessica.arvizuto@gmail.com>
2024-05-13 16:47:55 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
             Transform your brand
           </h2>
 
-          <p className='text-[18px] font-semibold'>
+          <p className='text-[18px] font-semibold pr-28 leading-relaxed	'>
             We are a full-service creative agency specializing in helping brands grow fast.
             Engage your clients through compelling visuals that do most of the marketing for you.
           </p>
@@ -43,19 +43,19 @@ function App() {
         <div>
           <img src={imgStandOut} alt='Coralish Pink glass cup on a darker coral background.' />
         </div>
-        <div className='flex flex-col justify-center items-center'>
+        <div className='flex flex-col justify-center space-y-10 pl-28'>
 
-        <h2 className='text-4xl font-black'>
+          <h2 className='text-[40px] font-black leading-[50px] pr-40'>
             Stand out to the right audience
           </h2>
 
-          <p>
+          <p className='text-[18px] font-semibold pr-36 leading-relaxed	'>
             Using a collaborative formula of designers, researchers, photographers,
             videographers, and copywriters, we’ll build and extend your brand in digital places.
           </p>
 
           <h3>
-            <a>
+            <a className='hover:underline uppercase font-black'>
               Learn more
             </a>
           </h3>
@@ -67,7 +67,7 @@ function App() {
           <img src={imgGraphicDesign} alt='Cherries on a green background.' />
 
           <div className="absolute top-80 left-0 right-0 bottom-0 flex flex-col justify-center items-center p-36">
-            <h2 className="text-3xl font-bold mb-4 font-black text-ss_dark-des-cyan">
+            <h2 className="text-3xl font-bold mb-4 mt-4 font-black text-ss_dark-des-cyan">
               Graphic Design
             </h2>
             <p className="font-bold px-12 mt-4 text-center leading-relaxed text-ss_dark-des-cyan">
@@ -81,7 +81,7 @@ function App() {
           <img src={imgPhotography} alt='Tangerine on a teal background.' />
 
           <div className="absolute top-80 left-0 right-0 bottom-0 flex flex-col justify-center items-center p-36">
-            <h2 className="text-3xl font-bold mb-4 font-black text-ss_dark-blue">
+            <h2 className="text-3xl font-bold mb-4 mt-4 font-black text-ss_dark-blue">
               Photography
             </h2>
             <p className="font-bold px-12 mt-4 text-center leading-relaxed text-ss_dark-blue">
